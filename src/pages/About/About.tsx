c2a1356804d10f3b3015d003481b09a4f3b4f811
Houston Braga <houstonliverboy123@gmail.com>
2025-02-28 21:41:49 -0300

    edit: edit opacity card about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -61,14 +61,14 @@ const About = () => {
             Por Que Escolher a{" "}
             <span className="text-amber-400">Atmos Digital</span>?
           </h1>
-          <div className="card-about rounded-md px-4 py-4 bg-zinc-900 mb-10">
+          <div className="card-about rounded-md px-4 py-4 bg-zinc-900 bg-opacity-60 mb-10">
             <CardAbout />
           </div>
           <p className="text-center max-w-[600px] mb-5">
             Vamos juntos criar experiências digitais que geram impacto! 🚀 Entre
             em contato conosco e transforme sua presença online.
           </p>
-          <div className="flex flex-col gap-5 items-center">
+          <div className="flex flex-col gap-5 items-center ">
             <MoveDown size={28} strokeWidth={3} className="text-amber-400" />
             <Button className="hover:border-amber-400 border border-amber-400">Clique para falar conosco!</Button>
           </div>
