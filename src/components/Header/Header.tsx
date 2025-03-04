f22d41714413a6b87819cb765121158a3a738ec7
Houston Braga <houstonliverboy123@gmail.com>
2025-03-03 23:52:02 -0300

    feat: add shadow and effects in the header scrool

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,59 @@
+import { useState, useEffect } from "react";
 import { GridContainer } from "../GridContainer";
 import { LinkItem } from "./LinkItem";
 
 const menuItens = [
   {
-    path: '/',
-    title: 'Inicio',
+    path: "/",
+    title: "Inicio",
   },
 
   {
-    path: '/sobre',
-    title: 'Sobre nós',
+    path: "/sobre",
+    title: "Sobre nós",
   },
 
   {
-    path: '/servicos',
-    title: 'Serviços',
+    path: "/servicos",
+    title: "Serviços",
   },
 
   {
-    path: '/projetos',
-    title: 'Projetos',
+    path: "/projetos",
+    title: "Projetos",
   },
 
   {
-    path: '/contatos',
-    title: 'Contatos',
+    path: "/contatos",
+    title: "Contatos",
   },
-
-]
+];
 
 export function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handlescrool = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handlescrool);
+    return () => window.removeEventListener("scroll", handlescrool);
+  }, []);
+
   return (
-    <header className="header py-3 top-0 w-full">
+    <header
+      className={`header sticky py-3 top-0 w-full transition-all duration-300 ${
+        scrolled ? "color-header shadow-lg" : "bg-transparent"
+      }`}
+    >
       <GridContainer className="flex justify-between items-center">
         <div>
-          <img src="/logo.svg" alt="logo-img" width={120} />
+          <img src="/logo.svg" alt="logo-img" width={100} />
         </div>
         <nav className="flex items-center gap-6">
           <ul className="flex gap-16">
