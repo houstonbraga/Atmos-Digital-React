81cfd8acd52e76bae3e0c622db85382b94642c4f
Houston Braga <houstonliverboy123@gmail.com>
2025-03-07 00:21:23 -0300

    feat: edit margin bottom SlideLogo

--- src/pages/Services/components/SlideLogo.tsx
+++ src/pages/Services/components/SlideLogo.tsx
@@ -1,6 +1,6 @@
 export function SlideLogo() {
   return(
-    <div className="mt-40 w-full flex gap-20 justify-center items-center">
+    <div className="mt-40 mb-40 w-full flex gap-20 justify-center items-center">
       <img src="./mark-services-logo.svg" />
       <img src="./mark-services-logo.svg" />
       <img src="./mark-services-logo.svg" />
