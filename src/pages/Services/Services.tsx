f65d4fb3c27209e863b5f04fd4795fa3672ae273
Houston Braga <houstonliverboy123@gmail.com>
2025-03-06 21:52:14 -0300

    feat: add slide and shadowns in the services section

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -3,12 +3,15 @@ import { Header } from "@/components/Header/Header";
 import "./index.css";
 import CardServices from "./components/CardServices";
 import { MoveDown } from "lucide-react";
+import { SlideLogo } from "./components/SlideLogo";
+import { ShadowBg } from "@/components/ShadowBg";
 
 export default function Services() {
   return (
     <div className="flex flex-col justify-center items-center relative ">
       <Header />
-
+      <ShadowBg variant="shadowCenter" className="z-[-1]"/>
+      <ShadowBg variant="shadowRight" className="z-[-1]"/>
       <GridContainer className="flex flex-col gap-5 justify-center items-center">
         <h1 className="text-5xl font-gurajada text-amber-400 mt-16">
           Nossos serviços
@@ -18,6 +21,8 @@ export default function Services() {
           <CardServices />
         </div>
       </GridContainer>
+      <SlideLogo />
+      <ShadowBg variant="shadowLeft" className="z-[-1] -bottom-8"/>
     </div>
   );
 }
---
+++ src/pages/Services/components/SlideLogo.tsx
@@ -0,0 +1,21 @@
+export function SlideLogo() {
+  return(
+    <div className="mt-40 w-full flex gap-20 justify-center items-center">
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+      <img src="./mark-services-logo.svg" />
+    </div>
+  )
+}
