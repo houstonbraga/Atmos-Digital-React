f40b1abd52320c8597a83366e3481974b8740c9d
Houston Braga <houstonliverboy123@gmail.com>
2025-04-10 16:22:57 -0300

    teste

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,8 +1,81 @@
 import { GridContainer } from "@/components/GridContainer";
 import { Header } from "../../components/Header/Header";
 import { HandDrawnWave } from "@/components/HandDrawnWave";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowDown, TrashIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+interface Person {
+  id: number;
+  name: string;
+}
+
+const initialItems: Person[] = [
+  {
+    id: 1,
+    name: "houston",
+  },
+  {
+    id: 2,
+    name: "theo",
+  },
+  {
+    id: 3,
+    name: "carol",
+  },
+];
+
+
+export function QueueList() {
+  const [queueName, setQueueName] = useState('')
+  const [queue, setQueue] = useState<Person[]>(initialItems);
+  
+  function handleAddQueueClick() {
+    setQueue([
+      {
+        id: queue.length + 1,
+        name: queueName,
+      },
+      ...queue,
+    ]);
+
+    setQueueName('');
+  }
+
+  function handleDeletePerson(personId: number) {
+    setQueue(queue.filter((person) => person.id !== personId));
+  }
+
+  return (
+    <div className="mt-20 flex flex-col items-center gap-5">
+      <h1>Coloque seu nome na fila </h1> <ArrowDown />
+      <label className="flex justify-center items-center gap-2">
+        <Input placeholder="Name" value={queueName} onChange={(e) => setQueueName(e.target.value)}/>
+        <Button variant="default" onClick={handleAddQueueClick}>Enviar</Button>
+      </label>
+
+      <ul className="flex flex-col items-center">
+        {queue.map((person => { 
+          return (
+            <div key={person.id} className="flex items-center gap-2 mb-3">
+              <li >{person.name}</li>
+              <Button
+                variant="outline"
+                onClick={() => handleDeletePerson(person.id)}
+              >
+                <TrashIcon className="text-amber-400" />
+              </Button>
+            </div>
+          )
+        }))}
+      </ul>
+    </div>
+  );
+}
 
 const Projects = () => {
+  
   return (
     <div className="flex flex-col justify-center items-center relative">
       <Header />
@@ -13,6 +86,7 @@ const Projects = () => {
           </h1>
           <HandDrawnWave />
         </div>
+        <QueueList />
       </GridContainer>
     </div>
   );
