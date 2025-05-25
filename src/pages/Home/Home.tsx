efdb1056db7f5a006cab6f582bed52a10cef3cad
Houston Braga <houstonliverboy123@gmail.com>
2025-05-25 02:07:13 -0300

    edit: edit overflow hidden in the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,7 +8,7 @@ import './index.css'
 import BackgroundHome from "./components/BackgroundHome";
 export function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen">
+    <div className="overflow-hidden relative flex flex-col items-center justify-center min-h-screen">
       <BackgroundHome />
       <Header />
       <ShadowBg variant="shadowCenter"/>
